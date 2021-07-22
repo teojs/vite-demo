0e62d1e6033f0f29238de52a0813d22dbaa86960
Teo <cheesen.xu@gmail.com>
2021-07-22 16:15:23 +0800

    init

---
+++ plugin/updateVersion/index.js
@@ -0,0 +1,11 @@
+const fs = require('fs')
+const path = require('path')
+const packagePath = path.join(process.cwd(), 'package.json')
+const packageContent = require(packagePath)
+
+// 自动升级版本号
+const newVersion = packageContent.version.split('.')
+newVersion[2]++
+packageContent.version = newVersion.join('.')
+
+fs.writeFileSync(packagePath, JSON.stringify(packageContent, null, 2))
